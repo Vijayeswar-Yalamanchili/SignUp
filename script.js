54cac7caca9487e3ad06eda3387bb9b5f9e86a7d
Vijayeswar-Yalamanchili <121041042+Vijayeswar-Yalamanchili@users.noreply.github.com>
2022-12-23 12:24:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function validation(){
+var text= document.getElementById("#email");
+var code =document.getElementById("#password")
+
+if(email.value === "vijay@gmail.com" && password.value ==="vj123456" ){
+    // return true;
+    console.log(true);
+}else{
+    // return false;
+    console.log(false);
+}
+}
